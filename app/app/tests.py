83cf8adb6e120a3159a7985c8fc864beded47f90
Josim Uddin <mdjosimuddin712@gmail.com>
2022-06-20 21:58:22 +0600

    link issue fixed

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,10 +1,7 @@
 """
 Sample tests
 """
-
-
 from django.test import SimpleTestCase
-
 from app import calc
 
 
@@ -14,12 +11,9 @@ class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
         """Test adding numbers together"""
         res = calc.add(5, 6)
-
         self.assertEqual(res, 11)
-
 
     def test_subtract_numbers(self):
         """Test subtracting number y from x"""
         res = calc.subtract(5, 6)
-
         self.assertEqual(res, -1)
