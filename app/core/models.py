16a08daf2b03d2156c4051884afb1e09c644c24b
Josim Uddin <mdjosimuddin712@gmail.com>
2022-07-02 18:23:45 +0600

    created new recipe app

--- app/core/models.py
+++ app/core/models.py
@@ -1,7 +1,7 @@
 """
 Database models
 """
-
+from django.conf import settings
 from pyexpat import model
 from unicodedata import name
 from django.db import models
@@ -43,4 +43,20 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
     USERNAME_FIELD = 'email'
-# Create your models here.
+
+
+class Recipe(models.Model):
+    """Recipe object"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
+
